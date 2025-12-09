f0ceb16d95f8d5041cd9c6ca6157ff8bc335c68e
Citizen Agim <altindag.agim@gmail.com>
2025-12-09 01:06:30 +0100

    fix: update mediaTypes for image picker to use string array format

--- app/(main)/players/[id]/index.tsx
+++ app/(main)/players/[id]/index.tsx
@@ -79,7 +79,7 @@ export default function PlayerDetailScreen() {
 
   const handlePickImage = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ['images'],
       allowsEditing: true,
       aspect: [1, 1],
       quality: 0.5,
--- app/(main)/players/new.tsx
+++ app/(main)/players/new.tsx
@@ -29,7 +29,7 @@ export default function NewPlayerScreen() {
 
   const handlePickImage = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ['images'],
       allowsEditing: true,
       aspect: [1, 1],
       quality: 0.5,
